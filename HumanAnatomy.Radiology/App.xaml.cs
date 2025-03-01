f998cf5fb60cd7598d6ff144859b260546e23b5b
AbalfazlEs <abalfazleses@gmail.com>
2025-03-01 15:12:27 +0330

    Initial commit

---
+++ HumanAnatomy.Radiology/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace HumanAnatomy.Radiology
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ HumanAnatomy.Radiology/BodyAnatomy.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HumanAnatomy.Radiology
+{
+    /// <summary>
+    /// Interaction logic for BodyAnatomy.xaml
+    /// </summary>
+    public partial class BodyAnatomy : UserControl
+    {
+        public BodyAnatomy()
+        {
+            InitializeComponent();
+        }
+    }
+}
